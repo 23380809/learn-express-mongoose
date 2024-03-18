77f115b1f901a7fca20a8c57d7cef2b74c0a0b1b
shawn <shawn310480@gmail.com>
2024-03-18 17:25:55 -0400

    031824

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,16 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+
+const get_all_available_books = () => {
+  return BookInstance.find({status: 'Available'})
+  .populate('book')
+  .select('book status')
+}
+
+exports.show_all_books_status = async function(res) {
+  const books = await get_all_available_books();
+
+  res.send(books.map((book) => {
+    return book.book.title + " : " + book.status;
+  }));
 }
